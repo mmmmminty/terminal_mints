de0336cb777aed72da10743e00faa0b8f6611794
Xavier <careyx1@careyx1.local>
2023-12-23 22:00:13 -0500

    Finsihed hangman v0.1..1? idk, its functional

--- mints_cli/src/hangman/mod.rs
+++ mints_cli/src/hangman/mod.rs
@@ -1,4 +1,9 @@
-use std::{time::{Instant, Duration}, error::Error};
+use std::{
+    collections::HashSet,
+    error::Error,
+    io::{BufRead, BufReader, Write},
+    time::{Duration, Instant},
+};
 
 use colored::Colorize;
 use terminal_size::{terminal_size, Width};
@@ -26,7 +31,7 @@ pub struct Hangman {
     pub answer: String,
     pub time_started: Instant,
     pub difficulty: Difficulty,
-    pub ascii: Vec<String>
+    pub ascii: Vec<String>,
 }
 
 impl Game for Hangman {
@@ -41,21 +46,74 @@ impl Game for Hangman {
             answer,
             time_started: std::time::Instant::now(),
             difficulty: args.difficulty.clone(),
-            ascii: vec![ASCII_0, ASCII_1, ASCII_2, ASCII_3, ASCII_4, ASCII_5, ASCII_6]
-                .iter()
-                .map(|s| s.to_string())
-                .collect()
+            ascii: vec![
+                ASCII_0, ASCII_1, ASCII_2, ASCII_3, ASCII_4, ASCII_5, ASCII_6,
+            ]
+            .iter()
+            .map(|s| s.to_string())
+            .collect(),
         }
     }
 
     fn start(&mut self) {
-
+        self.display(None);
+        // println!("Answer: {}", self.answer);
+        // println!("Hint: {}", hint(&self.answer));
     }
 
     fn do_loop(&mut self) -> Result<i32, Box<dyn Error>> {
-        self.display();
-        self.incorrect.push('c');
-        std::thread::sleep(Duration::from_secs(1));
+        let mut input = BufReader::new(std::io::stdin()).lines();
+        let guess = match input.next() {
+            Some(s) => s.unwrap().to_ascii_uppercase(),
+            None => return Ok(GAME_OVER),
+        };
+        let letter = guess.chars().nth(0).unwrap_or(';');
+
+        // Handle command and early return
+        if guess.starts_with('!') {
+            return Ok(self.handle_commands(&guess.to_ascii_lowercase()));
+        }
+
+        // Correct amount of letters
+        if guess.len() != 1 {
+            println!("Guess one letter at a time!");
+
+        // Is a ascii alphabetical
+        } else if !letter.is_ascii_alphabetic() {
+            println!("{letter} is not a letter silly!");
+
+        // Already guessed
+        } else if self.incorrect.contains(&letter) || self.correct.contains(&letter) {
+            println!("You've guesses '{letter}' already!");
+
+        // Otherwise go to next turn
+        } else {
+            if self.answer.contains(letter) {
+                self.correct.push(letter);
+            } else {
+                self.incorrect.push(letter);
+            }
+
+            if self.check_win() {
+                self.display(Some(true));
+            } else if self.incorrect.len() == HANGMAN_GUESS_SIZE as usize {
+                self.display(Some(false));
+            } else {
+                self.turn += 1;
+                self.display(None);
+                return Ok(GAME_ONGOING);
+            }
+
+            println!("Play again? (y/n)");
+            return match input.next() {
+                Some(s) => match s.unwrap().as_str() {
+                    "Y" | "y" => Ok(GAME_RESTART),
+                    _ => Ok(GAME_OVER),
+                },
+                None => Ok(GAME_OVER),
+            };
+        }
+
         Ok(GAME_ONGOING)
     }
 
@@ -66,10 +124,10 @@ impl Game for Hangman {
 }
 
 impl Hangman {
-    fn display(&self) {
+    fn display(&self, win: Option<bool>) {
         // Clear terminal
         print!("{}[2J", 27 as char);
-        println!();
+        newln!();
 
         let term_width = if let Some((Width(w), _)) = terminal_size() {
             w as usize
@@ -79,7 +137,15 @@ impl Hangman {
 
         let header_text = match self.turn {
             0 => " Hangman! ".to_string(),
-            _ => format!(" Round {} ", self.turn + 1)
+            _ => {
+                if win.is_none() {
+                    format!(" Round {} ", self.turn + 1)
+                } else if win.unwrap() {
+                    " You won! ".to_string()
+                } else {
+                    format!(" The word was {}! ", self.answer)
+                }
+            }
         };
 
         // Calculate padding
@@ -89,10 +155,75 @@ impl Hangman {
 
         // Print header with padding
         println!("{}{}{}", padding, header_text.bold(), padding);
-        println!();
+        newln!();
 
         // Print ascii art
-        print!("{}", self.ascii[self.incorrect.len()]);
+        if win.is_none() {
+            print!("{}", self.ascii[self.incorrect.len()]);
+        } else if win.unwrap() {
+            print!("{}", ASCII_WIN);
+        } else {
+            print!("{}", ASCII_LOSE);
+        }
+        newln!(2);
 
+        let bar = "=".repeat((padding_length * 2) + header_text.len());
+        println!("{bar}");
+        newln!();
+
+        // Print correct words
+        for c in self.answer.chars() {
+            if self.correct.contains(&c) {
+                print!(" {} ", c.to_string().green().bold());
+            } else {
+                print!(" {} ", "_".to_string().bold());
+            }
+        }
+
+        newln!(2);
+        println!("{bar}");
+        newln!();
+
+        for c in &self.incorrect {
+            print!(" {} ", c.to_string().red().bold());
+        }
+
+        newln!(2);
+    }
+
+    fn check_win(&self) -> bool {
+        let answer: HashSet<char> = self.answer.chars().collect();
+        let correct: HashSet<char> = self.correct.clone().into_iter().collect();
+
+        answer == correct
+    }
+
+    fn handle_commands(&mut self, cmd: &String) -> i32 {
+        match cmd.as_str() {
+            "!hint" | "!h" => {
+                println!("Hint: {}", hint(&self.answer));
+                GAME_ONGOING
+            }
+            "!restart" | "!next" | "!reset" | "!r" => {
+                println!("The word was {}!", self.answer);
+
+                print!("Restarting in 3.. ");
+                std::io::stdout().flush().expect("Failed to flush stdout");
+                std::thread::sleep(Duration::from_secs(1));
+                print!("2.. ");
+                std::io::stdout().flush().expect("Failed to flush stdout");
+                std::thread::sleep(Duration::from_secs(1));
+                print!("1.. ");
+                std::io::stdout().flush().expect("Failed to flush stdout");
+                std::thread::sleep(Duration::from_secs(1));
+
+                GAME_RESTART
+            }
+            "!quit" | "!leave" | "!exit" | "!q" => GAME_OVER,
+            _ => {
+                println!("Unknown command!");
+                GAME_ONGOING
+            }
+        }
     }
 }
